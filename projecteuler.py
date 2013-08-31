34052b927ff4b603b6e5116b01b9bf5f36efbe1c
simonwillcock <simon@willcock.com.au>
2013-09-01 02:20:15 +0300

    Initial Commit'

---
+++ projecteuler.py
@@ -0,0 +1,7 @@
+def Problem1:
+	# sum of multiples of 3 and 5 up to 1000
+	sum([x for x in range(1000) if x % 3 == 0 or x % 5 == 0])
+
+def Problem2:
+	# Sum of even fibonnaci numbers less than 4,000,000
+	pass
